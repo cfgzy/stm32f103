fdde6101370ef7218b9a3dd91b6e288fd2159a13
zy <cfgzy03@gmail.com>
2026-07-07 21:44:02 +0800

    chore: update esp8266 configuration

--- HARDWARE/esp8266/esp8266.h
+++ HARDWARE/esp8266/esp8266.h
@@ -4,14 +4,14 @@
     #include "sys.h"
 
     // ================== 常用宏定义（用户需根据实际情况修改） ==================
-    #define WIFI_SSID         "YOUR_WIFI_SSID"                     // WiFi 名称
-    #define WIFI_PASSWORD     "YOUR_WIFI_PASSWORD"                // WiFi 密码
+    #define WIFI_SSID         "aaa"                     // WiFi 名称
+    #define WIFI_PASSWORD     "1111"                // WiFi 密码
     // 注意：这里是巴法云的私钥，作为Client ID使用
-    #define MQTT_CLIENT_ID    "768166ef5bdc47a0be78f4e67514b277"
+    #define MQTT_CLIENT_ID    "1111"
     #define MQTT_BROKER       "mqtt.bemfa.com"          // MQTT 服务器地址
     #define MQTT_PORT         "9501"                    // MQTT 端口号
-    #define MQTT_PUB_TOPIC    "YOUR_BEMFA_PUB_TOPIC"                 // 温湿度上报主题
-    #define MQTT_SUB_TOPIC    "YOUR_BEMFA_SUB_TOPIC"                // 云端控制命令主题
+    #define MQTT_PUB_TOPIC    "esp02up"                 // 温湿度上报主题
+    #define MQTT_SUB_TOPIC    "esp02cmd"                // 云端控制命令主题
 
     // ================== 外部变量声明 ==================
     extern u8 mqtt_connected;        /* MQTT连接状态（在main.c中定义） */
